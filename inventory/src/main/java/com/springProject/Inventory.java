a7f51d7fc39bfe36cd3c3ae208603432795f5946
Harshal Gosavi <harshal.gosavi@logicmonitor.com>
2025-03-30 14:21:56 +0530

    Implemented service registry

--- inventory/src/main/java/com/springProject/Inventory.java
+++ inventory/src/main/java/com/springProject/Inventory.java
@@ -2,11 +2,12 @@ package com.springProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class Inventory {
     public static void main(String[] args) {
         SpringApplication.run(Inventory.class, args);
